c4da84f8798fc7884e57a213d2ea8cf65c03ed69
diegobauto <diegobauto00@gmail.com>
2024-09-02 23:19:21 -0500

    feat: API CRUD serverless crud plantillas y tipo

---
+++ src/handlers/health/app.py
@@ -0,0 +1,12 @@
+# Health Check to API Gateway and lambda
+import json
+
+def lambda_handler(event, context):
+    return {
+        "statusCode": 200,
+        "body": json.dumps({
+            "Success": True,
+            "Status": 200,
+            "Message": "Plantillas crud, funciones lambda con SAM"
+        })
+    }
